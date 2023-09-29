5109a8a9aef66094d18905fa0cfac30e746adfe2
Kihito <benner.andrey.loon@gmail.com>
2023-09-29 18:34:02 +0300

    Prisma and db

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { Express, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
 import cors from "cors";
 import { UsersController } from "./controllers/users.controller";
+import prisma from "./db/prisma";
 
 dotenv.config();
 
@@ -9,28 +11,42 @@ const PORT = process.env.PORT || 3000;
 
 class App {
   private app: Express;
+  private prisma: PrismaClient;
+
   constructor() {
+    /* App express */
     this.app = express();
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.static("public"));
     this.app.use(express.json());
     this.app.use(cors());
+
+    /* DB */
+    this.prisma = prisma;
   }
 
   public __init__ = async () => {
     try {
+
+
       this.app.listen(PORT, () => {
         console.log("Server started");
       });
 
-      this.app.use("/users", new UsersController().getRouter());
+      /* Endpoints */
+      this.app.use("/api/users", new UsersController(this.prisma).getRouter());
+
+
+      process.on("beforeExit", async () =>{
+        await this.prisma.$disconnect();
+      });
     } catch (error: unknown) {
         const err = error as Error;
         console.log(err.message);
     }
-  };
+  }
 }
-
+console.clear();
 export const app = new App();
 
 app.__init__().then(() => {
